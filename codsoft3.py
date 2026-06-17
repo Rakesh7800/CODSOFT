d449dce0f2bc5f23ee73e766b56cf11157dd3556
Rakesh <dokkarakesh88@gmail.com>
2026-06-17 11:22:19 +0530

    Add files via upload

---
+++ codsoft3.py
@@ -0,0 +1,17 @@
+﻿import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+# Initializing a redesigned pipeline
+neural_net = models.wide_resnet50_2(pretrained=True)
+neural_net.eval()
+
+pipeline = transforms.Compose([
+    transforms.Resize(280),
+    transforms.CenterCrop(256),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.495, 0.465, 0.420], std=[0.210, 0.205, 0.215]),
+])
+
+print("Vision Model Initialized Successfully.")
+
